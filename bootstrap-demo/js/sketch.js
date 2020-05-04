ee37b574ef0ab7befeff605ccb13758d66ed37e2
L A R K <lark.buckingham@sjsu.edu>
2020-05-03 21:28:05 -0700

    bootstrap demo complete

---
+++ bootstrap-demo/js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  fill(146, 214, 164);
+  noStroke();
+}
+
+function draw() {
+  // background(220);
+  clear();
+  ellipse(mouseX, mouseY, 66); // draws circle at cursor
+}

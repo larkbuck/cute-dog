cabd72c8fa80cbdbdf3e80c89c9debcd593e98dc
L A R K <lark.buckingham@sjsu.edu>
2020-05-05 19:25:37 -0700

    added paw prints

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,22 @@
+let paw;
+
+function preload() {
+  paw = loadImage('assets/paw-grey.png');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  fill(146, 214, 164);
-  noStroke();
+  // draw a paw at 1 second intervals
+  setInterval(function() {
+    let pawSize = random(15, 30);
+    image(paw, random(windowWidth), random(windowHeight), pawSize, pawSize);
+  }, 1000)
+
 }
 
 function draw() {
-  // background(220);
-  clear();
-  ellipse(mouseX, mouseY, 66); // draws circle at cursor
+  // background(220); // draws background fill
+  // clear(); // clears background
+  // ellipse(mouseX, mouseY, 30); // draws circle at cursor
 }
